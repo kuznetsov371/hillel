fab1413dcdc61a35e9ebe5a6da501447b02e2a58
kuznetsov371 <kuzya07272@gmail.com>
2022-02-19 00:35:26 +0200

    second commit with hw

--- utils/logger.js
+++ utils/logger.js
@@ -1,18 +1,15 @@
 const chalk = require("chalk");
 
 function info(...args){
-    debugger;
-    return console.log(chalk["green"](...args));
+    console.log(chalk.green(...args));
 }
 
 function warn(...args){
-    debugger;
-    return console.log(chalk["yellow"](...args));
+    console.log(chalk.yellow(...args));
 }
 
 function error(...args){
-    debugger;
-    return console.log(chalk["red"](...args));
+    console.log(chalk.red(...args));
 }
 
 module.exports = {
